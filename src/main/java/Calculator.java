242b0cfd3453deebc3eb9e6bb9bde9cf20be5d08
kotlasrinivasreddy <ksrinivasreddy4444@gmail.com>
2021-03-10 15:10:36 +0530

    adding test cases

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Calculator {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         System.out.println("welcome to my calculator");
         while(true) {
             System.out.print("press  1--for square root operation    2--for factorial    3--for logarithm");
@@ -14,7 +14,11 @@ public class Calculator {
             if (keyPressed == 1) {
                 System.out.println("enter the number to calculate square root: ");
                 double num = Double.parseDouble(br.readLine());
-                squareRoot(num);
+                double result= squareRoot(num);
+                if(result>=0)
+                    System.out.println("square root of "+num+" is: "+squareRoot(num));
+                else
+                    System.out.println("real square root exists only for positive numbers");
             }
             else if (keyPressed == 2) {
                 System.out.println("enter the number to calculate factorial: ");
@@ -24,7 +28,11 @@ public class Calculator {
             else if (keyPressed == 3) {
                 System.out.println("enter the number to calculate natural logarithm(base e): ");
                 double num = Double.parseDouble(br.readLine());
-                cal_natural_log(num);
+                if(num<0)
+                    System.out.println("enter positive numbers only");
+                else
+                    System.out.println("natural log value of "+num+" is: "+cal_natural_log(num));
+
             }
             else if (keyPressed == 4) {
                 System.out.println("enter the base and exponent to calculate the power of base value: ");
@@ -33,7 +41,7 @@ public class Calculator {
                 base_num = Double.parseDouble(br.readLine());
                 System.out.println("enter the exponent value: ");
                 exp_num = Double.parseDouble(br.readLine());
-                cal_pow(base_num, exp_num);
+                System.out.println(base_num + " to the power "+exp_num+ " is: "+cal_pow(base_num, exp_num));
             }
             else{
                 System.out.println("press 1 to use calculator again\npress any other number to exit");
@@ -44,31 +52,33 @@ public class Calculator {
             }
         }// end of infinite while loop
     }
-    public static void squareRoot(double num){
+    public static double squareRoot(double num){
         if(num<0)
-            System.out.println("real square root exists only for positive numbers");
-        else
-            System.out.println("square root of "+num+" is: "+Math.sqrt(num));
+            return Double.NaN;
+        return Math.sqrt(num);
     }
-    public static int factorial(int n)
+    public static double factorial(int n)
     {
         if(n<0){
             System.out.println("factorial doesn't exist for negative numbers");
-            return -1;
+            return Double.NaN;
         }
         if (n == 0)
             return 1;
 
         return n*factorial(n-1);
     }
-    public static void cal_natural_log(double num){
-        if(num<0)
-            System.out.println("enter positive numbers only");
-        else
-            System.out.println("natural log value of "+num+" is: "+Math.log(num));
+    public static double cal_natural_log(double num){
+        if(num==0)
+            return Double.NEGATIVE_INFINITY;
+        else if(num<0)
+            return Double.NaN;
+        return Math.log(num);
     }
-    public static void cal_pow(double base_num, double exp_num){
-        System.out.println(base_num + " to the power "+exp_num+ " is: "+Math.pow(base_num, exp_num));
+    public static double cal_pow(double base_num, double exp_num){
+        if(base_num==0 && exp_num==0)
+            return Double.NaN;
+        return Math.pow(base_num, exp_num);
     }
 
 }
